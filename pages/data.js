81c0ea93234ca9655149cc032d122a3bb915c418
Laura Padilla <hi@lauracodes.com>
2021-05-15 17:47:57 -0400

    update that data

--- pages/data.js
+++ pages/data.js
@@ -8,7 +8,7 @@ const rankings = [
   {
     id: 2,
     rank: "Private PITA",
-    info: "Private Pitas are still considered mild but slightly more annoying than their cadet counterparts. These little suckers are generally disguised as an obnoxious neighbor mowing the lawn at 9am.",
+    info: "Private Pitas are still considered mild but slightly more annoying than their cadet counterparts. These little suckers are generally disguised as an obnoxious neighbor mowing the lawn at 9am or this annoying marquee text scrolling through the middle of the page.",
     stat: "Mild",
   },
   {
@@ -20,37 +20,37 @@ const rankings = [
   {
     id: 4,
     rank: "Lieutenant PITA",
-    info: "This is where things get interesting. Lieutenant Pitas are inconvenient enough to completely derail your focus and/or ruin your day. Lieutenants can be things like a store not offering free returns or free shipping, or your Amazon package being delayed by a few days.",
+    info: "This is where things get interesting. Lieutenant Pitas are inconvenient enough to completely derail your focus and/or ruin your day. Lieutenants can be things like being hangry and realizing Chick Fil A is closed on Sundays, or someone scheduling a meeting at 8am.",
     stat: "Moderate",
   },
   {
     id: 5,
     rank: "Captain PITA",
-    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    info: "Captain Pitas are named afted Capt'n Crunch. If you've ever had Capt'n Crunch you'll know what this means and why it's appropriate. Captains can also be finding out a store doesn't offer free returns or free shipping on something you need but could probably get at the mall. Just drive you lazy SOB. ",
     stat: "Moderate",
   },
   {
     id: 6,
-    rank: "General PITA",
-    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    rank: "Commander PITA",
+    info: "A Commander is a sneaky SOB. Like needing to go to the bathroom in the middle of a concert while they play your favorite song because you drank a 60oz bottle of Coke. This one's on you. ",
     stat: "Moderate",
   },
   {
     id: 7,
     rank: "Admiral PITA",
-    info: "Admirals are on another level. Imagine your amazon packages constantly getting lost in the mail.",
+    info: "Admirals are on another level. Equate this to your packages constantly getting lost in the mail, someone rear-ending you, and they don't have insurance, or not being able to figure out why the code isn't working!!! Ultimate PITA.",
     stat: "Extreme",
   },
   {
     id: 8,
     rank: "Supreme Leader PITA",
-    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    info: "This is the mother all of inconveniences, like Kim Jong-un, Vladimir Putin and Trump joining forces to specifcially ruin your life. Supreme Leader PITAs are hard to ignore, you will most likely need to stop what you're doing and take care of this a-hole, it will not go away until resolved. Or gets overthrown by a coup.",
     stat: "Extreme",
   },
   {
     id: 9,
     rank: "Grand Czar PITA",
-    info: "Bruh just move to Peru and befriend alpacas.",
+    info: "Bruh just forget about everything and move to Peru and be one with the alpacas.",
     stat: "Extreme",
   },
 ];
